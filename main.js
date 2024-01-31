f1f1ab9059bbefa5d40e51232b308a9a23c466d8
Alejandro Perez Gaimberan <alejandropg@hotmail.com.ar>
2024-01-30 23:23:11 -0300

    final

--- main.js
+++ main.js
@@ -1,89 +1,57 @@
-const productos = [
-    {
-      id: 1,  
-      nombre: "guitarra electrica",
-      precio: 600,
-      imagen: "./img/guitarraElectrica.jpg"
-    },
-    {
-      id: 2,  
-      nombre: "guitarra criolla",
-      precio: 500,
-      imagen: "./img/guitarraCriolla.jpg"
-    },
-    {
-        id: 3,  
-      nombre: "guitarra acústica",
-      precio: 550,
-      imagen: "./img/guitarraAcustica.jpg"
-    },
-    {
-        id: 4,  
-      nombre: "bajo eléctrico",
-      precio: 580,
-      imagen: "./img/bajoElectrico.jpg"
-    },
-    {
-        id: 5,  
-      nombre: "micrófono",
-      precio: 350,
-      imagen: "./img/microfono.jpg"
-    },
-    {
-        id: 6,  
-      nombre: "batería",
-      precio: 750,
-      imagen: "./img/bateria.jpg"
-    },
-    {
-        id: 7,  
-      nombre: "teclado",
-      precio: 650,
-      imagen: "./img/pianoElectrico.jpg"
-    },
-  ];
+fetch('./data.json')
+    .then(response => response.json())
+    .then(data => {
+        const productos = data;
 
+const categorias = [];
+let containerProductos = document.getElementById("productos");
+let containerCarrito = document.getElementById("containerCarrito");
 
-productos.forEach((item) => {
-        let div = document.createElement("div");
-        div.innerHTML = `
-            <div class="card" style="width: 18rem; text-align: center;">
-            <img src="${item.imagen}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title">${item.nombre}</h5>
-            <p class="card-text">$${item.precio}</p>
-            <button id="boton${item.id}" class="btn btn-primary">Comprar</button>
-            </div>
-            </div>
-        `;
-        document.body.append(div);
-
-        let boton = document.getElementById(`boton${item.id}`);
-        boton.addEventListener("click", () => agregar(item.id));
-})
-
-
-JSON.parse(sessionStorage.getItem("carrito")) === null && sessionStorage.setItem("carrito", JSON.stringify([]));
-
-let carrito = JSON.parse(sessionStorage.getItem("carrito"));
-let divCarrito = document.createElement("div");
-
-const mostrarCarrito = () => {
-  divCarrito.innerHTML = ``;
-    carrito.forEach((producto) => {
-      divCarrito.innerHTML += `
-      <table class="table table-striped">
-      <tbody>
-        <tr>
-          <th scope="row">Producto: ${producto.nombre}</th>
-          <td>Precio unitario: $${producto.precio}</td>
-          <td>Cantidad: ${producto.cantidad}</td>
-          <td>Precio total: $${producto.precio * producto.cantidad} </td>
-        </tr>
-      `;
-      document.body.append(divCarrito);
-      })
+productos.forEach((producto) => {
+    if (!categorias.includes(producto.categoria)) {
+        categorias.push(producto.categoria);
     }
+});
+
+categorias.forEach((categoria) => {
+    let divCategoria = document.createElement("div");
+    divCategoria.innerHTML = `<h2>${categoria}</h2>`;
+    divCategoria.setAttribute(`id`, `${categoria}`);
+    divCategoria.classList.add(`row`, 'mb-4');
+    divCategoria.style.justifyContent = "center";
+    containerProductos.append(divCategoria);
+
+    const productosCategoria = productos.filter((producto) => producto.categoria === categoria);
+    productosCategoria.forEach((item) => {
+        let divProducto = document.createElement("div");
+        divProducto.classList.add(`col-lg-4`);
+        divProducto.innerHTML = `
+        <div class="card" style="margin-bottom: 2rem;"> 
+        <img src="${item.imagen}" class="card-img-top" alt="${item.nombre}" style="padding: 1rem;">
+        <div class="card-body" style="margin: 0 auto;">
+        <h5 class="card-title" >${item.nombre}</h5>
+        <p class="card-text" >$${item.precio}</p>
+        <p class="card-text" >${item.texto}</p>
+        <button type="button" class="btn btn-danger" id="boton${item.id}">Comprar</button>
+        </div>
+        </div>
+        `;
+        divCategoria.append(divProducto);
+        let boton = document.getElementById(`boton${item.id}`);
+        boton.addEventListener("click", () => {
+        agregar(item.id);
+          Swal.fire({
+          text: `Se ha agregado ${item.nombre} al carrito`,
+          icon: "success",
+        });
+        let card = boton.closest('.card');
+        card.classList.add(`colorComprar`)
+        setTimeout(() => {
+          card.classList.remove('colorComprar')
+      }, 3000)
+      });
+  });
+});
 
 const agregar = (id) => {
   let producto = productos.find((item) => item.id === id);
@@ -96,7 +64,7 @@ const agregar = (id) => {
       cantidad: 1,
     }
     carrito.push(nuevoProductoCarrito);
-
+  
     sessionStorage.setItem("carrito", JSON.stringify(carrito))
   }else{
     const indexProductoCarrito = carrito.findIndex((producto) => producto.id === id)
@@ -104,11 +72,143 @@ const agregar = (id) => {
     sessionStorage.setItem("carrito", JSON.stringify(carrito))
   }
   carrito = JSON.parse(sessionStorage.getItem("carrito"))
-
-  alert(`Usted compró el producto ${producto.nombre}`)
-  console.log(carrito)
   mostrarCarrito()
+  };
+
+})
+.catch(error => console.error('Error al cargar el archivo JSON:', error));
+
+JSON.parse(sessionStorage.getItem("carrito")) === null && sessionStorage.setItem("carrito", JSON.stringify([]));
+
+let carrito = JSON.parse(sessionStorage.getItem("carrito"));
+let divCarrito = document.createElement("div");
+
+const vaciarCarrito = () => {
+  carrito.forEach((producto) => {
+    producto.cantidad = 0;
+  });
+  let totalCarrito = 0;
+  carrito = [];
+  sessionStorage.setItem("carrito", JSON.stringify(carrito));
+  mostrarCarrito();
+  Toastify({
+    text: "Se ha vaciado el carrito correctamente.",
+    duration: 3000
+    }).showToast();
+  };
+
+const restarProducto = (id) => {
+  const indexProductoCarrito = carrito.findIndex((producto) => producto.id === id);
+  if (carrito[indexProductoCarrito].cantidad > 1) {
+    carrito[indexProductoCarrito].cantidad--;
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+    mostrarCarrito();
+    Toastify({
+      text: `Se ha quitado un ${carrito[indexProductoCarrito].nombre} del carrito.`,
+      duration: 3000
+      }).showToast();
+  } else {
+    carrito.splice(indexProductoCarrito, 1);
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+    mostrarCarrito();
+    Toastify({
+      text: `Se ha quitado ${carrito[indexProductoCarrito].nombre} del carrito.`,
+      duration: 3000
+      }).showToast();
+  }
 };
+
+const sumarProducto = (id) => {
+  const indexProductoCarrito = carrito.findIndex((producto) => producto.id === id);
+  carrito[indexProductoCarrito].cantidad++;
+  sessionStorage.setItem("carrito", JSON.stringify(carrito));
+  mostrarCarrito();
+  Swal.fire({
+    text: `Se ha agregado un ${carrito[indexProductoCarrito].nombre} al carrito`,
+    icon: "success",
+  });
+};
+
+const mostrarCarrito = () => {
+divCarrito.innerHTML = ``;
+let totalCarrito = 0; 
+
+carrito.forEach((producto) => {
+  const precioTotal = producto.precio * producto.cantidad;
+  totalCarrito += precioTotal; 
+
+  divCarrito.innerHTML += `
+    <table class="table table-striped" id="tablaCarrito">
+      <tbody>
+        <tr>
+          <th scope="row">Producto: ${producto.nombre}</th>
+          <td>Precio unitario: $${producto.precio}</td>
+          <td>Cantidad: ${producto.cantidad}</td>
+          <td>Precio total: $${precioTotal}</td>
+          <td><button id="restar${producto.id}" onclick="restarProducto(${producto.id})">-</button></td>
+          <td><button id="sumar${producto.id}" onclick="sumarProducto(${producto.id})">+</button></td>
+        </tr>
+      </tbody>
+    </table>`;
+});
+
+divCarrito.innerHTML += `
+  <table class="table table-striped">
+    <tbody>
+      <tr>
+        <th scope="row">Precio total del carrito: $${totalCarrito}</th>
+        <td><button id="vaciarCarrito" onclick="vaciarCarrito()">Vaciar carrito</button></td>
+      </tr>
+    </tbody>
+  </table>`;
+
+containerCarrito.append(divCarrito);
+
+};
+
+
+
+function normalize(text) {
+return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
+let search = document.getElementById("search");
+let botonSearch = document.getElementById("botonSearch");
+botonSearch.addEventListener("click", () => {
+event.preventDefault();
+let resultadoBusqueda = productos.filter((el) =>
+normalize(el.nombre.toLowerCase()).includes(normalize(search.value.toLowerCase()))
+);
+containerProductos.innerHTML = "";
+containerProductos.classList.add(`row`);
+containerProductos.style.justifyContent = "center";
+resultadoBusqueda.forEach((item) => {
+  let divBusqueda = document.createElement("div");
+  divBusqueda.classList.add(`col-lg-4`);
+  divBusqueda.innerHTML = `
+    <div class="card" style="margin-top: 2rem; margin-bottom: 2rem;"> 
+      <img src="${item.imagen}" class="card-img-top" alt="${item.nombre}">
+      <div class="card-body">
+        <h5 class="card-title">${item.nombre}</h5>
+        <p class="card-text">$${item.precio}</p>
+        <p class="card-text">${item.texto}</p>
+        <button type="button" class="btn btn-danger" id="boton${item.id}" >Comprar</button>
+      </div>
+    </div>
+  `;
+  containerProductos.append(divBusqueda);
+
+let boton = document.getElementById(`boton${item.id}`);
+boton.addEventListener("click", () => {
+agregar(item.id);
+  Swal.fire({
+  text: `Se ha agregado ${item.nombre} al carrito`,
+  icon: "success",
+});
+});
+});
+let carrouselCategorias = document.getElementById("carouselExampleCaptions");
+carrouselCategorias.style.display = "none";
+});
 
 
 
